013d8a5277eeb5f2f9f3fad5f38e635290ad65bd
WinterDyzrupt <winter@dyzruptgames.com>
2026-08-19 16:25:35 -0700

    Created Initial glowy button. (#5)
    * Created Initial glowy button.
    
    Updated Ignore to exclude TextMesh Pro Assets.
    
    * Addressed the comments.

---
+++ Assets/Common/Data/BoolWrapper.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+
+namespace Common.Data
+{
+    [CreateAssetMenu(fileName = "BoolWrapper", menuName = "Data/Bool Wrapper")]
+    public class BoolWrapper : ScriptableObject
+    {
+        [SerializeField] private bool currentValue;
+        [SerializeField] public bool defaultValue;
+
+        public event Action Changed;
+
+        private void Awake()
+        {
+            currentValue = defaultValue;
+        }
+
+        public void Set(bool newBool)
+        {
+            if (currentValue == newBool) return;
+            currentValue = newBool;
+            Changed?.Invoke();
+        }
+
+        [ContextMenu("Toggle")]
+        public void Toggle()
+        {
+            currentValue = !currentValue;
+            Changed?.Invoke();
+        }
+
+        public static implicit operator bool(BoolWrapper value) => value.currentValue;
+    }
+}
