c4c21789cf8c6a8ed8be119a58230184788a8201
Kajure <mjmthere@gmail.com>
2026-09-05 23:02:06 -0700

    Toggle clues together and separately. (#11)

--- Assets/Common/MonoBehaviours/ClickableClue.cs
+++ Assets/Common/MonoBehaviours/ClickableClue.cs
@@ -7,11 +7,23 @@ namespace Common.MonoBehaviours
 {
     public class ClickableClue : MonoBehaviour
     {
-        [SerializeField] private Image buttonImage;
-        [SerializeField] private Image glowImage;
-        [SerializeField] private Button button;
-        [SerializeField] private BoolWrapper isInteractable;
-        // [SerializeField] private ??? Something to trigger when clicked;
+        public Image buttonImage;
+        public Image glowImage;
+        public Button button;
+
+        public bool canBeInteractedWithMultipleTimes;
+
+        /// <summary>
+        /// Whether this specific clue is interactable.  This is set to false when this clue is clicked, preventing
+        /// this clue from being clicked again.
+        /// </summary>
+        public BoolWrapper isInteractable;
+        
+        /// <summary>
+        /// Whether any/all clues are interactable.  This is set to false when dialog or the mind palace are visible.
+        /// This prevents clues from glowing/etc. when something else is happening.
+        /// </summary>
+        public BoolWrapper cluesAreInteractable;
 
         [Header("Glow Variables")]
         public Color glowColor;
@@ -19,37 +31,50 @@ namespace Common.MonoBehaviours
         [Range(0, 1)] public float pulseSize = 0.2f;
         [Range(0,1)] public float animateSpeed = 0.4f;
 
-        private bool _isAnimating = false;
+        private bool _isAnimating;
         private Vector3 MinGlowSize => new Vector3(glowSize, glowSize, 0);
         private Vector3 FullPulseSize => new Vector3(glowSize + pulseSize, glowSize + pulseSize, 0);
     
         private void Awake()
         {
-            Debug.Assert(buttonImage != null, "Button Image wasn't assigned.");
-            Debug.Assert(glowImage != null, "Glow Image wasn't assigned.");
-            Debug.Assert(button != null, "Button wasn't assigned.");
-            Debug.Assert(isInteractable != null, "Interactable wasn't assigned.");
-        
+            Debug.Assert(buttonImage != null, $"{nameof(buttonImage)} wasn't assigned.");
+            Debug.Assert(glowImage != null, $"{nameof(glowImage)} wasn't assigned.");
+            Debug.Assert(button != null, $"{nameof(button)} wasn't assigned.");
+            Debug.Assert(isInteractable != null, $"{nameof(isInteractable)} wasn't assigned.");
+            Debug.Assert(cluesAreInteractable != null, $"{nameof(cluesAreInteractable)} wasn't assigned.");
+
             glowImage.sprite = buttonImage.sprite;
             glowImage.color = glowColor;
-        
+
+            cluesAreInteractable.Changed += SetInteractable;
             isInteractable.Changed += SetInteractable;
-            
+
             SetInteractable();
         }
 
         private void OnDestroy()
         {
+            cluesAreInteractable.Changed -= SetInteractable;
             isInteractable.Changed -= SetInteractable;
         }
 
         public void OnClueClicked()
         {
-            isInteractable.Set(false);
-            // TODO: Setup to do something
+            if (!canBeInteractedWithMultipleTimes)
+            {
+                isInteractable.Set(false);
+            }
         }
 
-        private void SetInteractable() => SetInteractable(isInteractable);
+        /// <summary>
+        /// Sets this clue to be interactable based on the clue-specific flag and the all-clues flag.
+        /// </summary>
+        private void SetInteractable()
+        {
+            var isThisClueInteractable = isInteractable && cluesAreInteractable; 
+
+            SetInteractable(isThisClueInteractable);
+        }
 
         private void SetInteractable(bool value)
         {
